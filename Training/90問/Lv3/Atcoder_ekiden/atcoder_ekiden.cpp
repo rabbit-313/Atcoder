b043f199a015770ba924ec1dce7a1b80ae8acfc1
rabbit0624 <migiso1051@i.softbank.jp>
2023-05-19 21:09:09 +0900

    順序列全探索

---
+++ Training/90問/Lv3/Atcoder_ekiden/atcoder_ekiden.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector <vector<int> > A(N, vector<int>A);
+    vector <int> X(M);
+    vector <int> Y(M);
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cin >> A.at(i).at(j)
+        }
+    }
+
+    int M;
+    cin >> M;
+    for(int i=0; i<M; i++){
+        cin >> X.at(i) >> Y.at(i);
+    }
+
+}
